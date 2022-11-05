0614f1b432e89afa97872496eeff82e73f56e404
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-11-05 20:31:51 +0800

    update docject.js

--- src/docject.js
+++ src/docject.js
@@ -1,5 +1,11 @@
 // https://github.com/dlvdls18/DocJect
 // tag, content, hasInner, attr
 var DocJect = {
-  toJSON(
+  toJSON(arg) {
+    var targets = [];
+    if(arg instanceof HTMLElement) targets.push(arg);
+    else {
+      var local = document.createElement("div");
+    }
+  }
 }
